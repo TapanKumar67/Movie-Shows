32d221d5889ad216a6694ddefd1b2044b1f13180
Tapan <tapankumark69@gmail.com>
2023-10-24 10:07:37 +0530

    First Commit of MovieList

---
+++ kilowott/kilowott/Controller/ProfilePopUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfilePopUpViewController.swift
+//  kilowott
+//
+//  Created by Tapan Kumar on 22/10/23.
+//
+
+import UIKit
+import Kingfisher
+
+class ProfilePopUpViewController: UIViewController {
+    
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    let defaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profileImageView.kf.indicatorType = .activity
+        
+        DispatchQueue.main.async {
+            self.profileImageView.kf.setImage(with: self.defaults.url(forKey: "profilePicture"))
+            self.nameLabel.text = "Welcome, \(self.defaults.string(forKey: "userName") ?? "")!"
+        }
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
+        UserDefaults.standard.set(false, forKey: "isShowProfile")
+        self.dismiss(animated: true)
+    }
+}
---
+++ kilowott/kilowott/Model/Movie.swift
@@ -0,0 +1,32 @@
+//
+//  Movie.swift
+//  Popular Movies
+//
+//  Created by Tapan Kumar on 21/10/23.
+//  Copyright © 2023 Tapan. All rights reserved.
+//
+
+import Foundation
+
+struct MoviesData: Decodable {
+    var movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies = "results"
+    }
+}
+
+struct Movie: Decodable {
+    let title: String?
+    let year: String?
+    let posterImage: String?
+    let backdropImage: String?
+    let overview: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, overview
+        case year = "release_date"
+        case posterImage = "poster_path"
+        case backdropImage = "backdrop_path"
+    }
+}
