c20dc32987b57f5e3582701d3cd6fd7121c98f61
Tapan <tapankumark69@gmail.com>
2023-12-21 16:43:44 +0530

    revert changes

--- kilowott/kilowott/Model/Movie.swift
+++ kilowott/kilowott/Model/Movie.swift
@@ -7,3 +7,28 @@
 //
 
 
+import Foundation
+
+struct MoviesData: Decodable {
+    var movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies = "results"
+    }
+}
+
+struct Movie: Decodable {
+    let title: String?
+    let year: String?
+    let posterImage: String?
+    let backdropImage: String?
+    let overview: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, overview
+        case year = "release_date"
+        case posterImage = "poster_path"
+        case backdropImage = "backdrop_path"
+    }
+}
+
